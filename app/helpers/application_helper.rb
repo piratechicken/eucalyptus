b17d5a2f7c360e94b34f4b6dcd6784d90900b25e
Simbidion <simonadwyer@yahoo.com.au>
2017-11-06 15:38:10 +1100

    money added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    # Takes 10000 cents and output is as $100.00
+  def format_money(cents)
+    money = Money.new(cents, "AUD")
+    money.format
+  end
+
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ApplicationRecord
   include ImageUploader::Attachment.new(:image)
-  
+
   belongs_to :user
   belongs_to :specie
 end
